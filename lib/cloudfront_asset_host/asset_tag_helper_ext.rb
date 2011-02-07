644abb4f3e266cc5a66dfc7c18a720e4cea2a5ac
Vadim Geshel <vadim@yummly.com>
2011-02-07 13:27:43 -0800

    undo debug output

--- lib/cloudfront_asset_host/asset_tag_helper_ext.rb
+++ lib/cloudfront_asset_host/asset_tag_helper_ext.rb
@@ -7,14 +7,12 @@ module ActionView
       # Override asset_id so it calculates the key by md5 instead of modified-time
       def rails_asset_id_with_cloudfront(source)
         if @@cache_asset_timestamps && (asset_id = @@asset_timestamps_cache[source])
-          puts "rails_asset_id_with_cloudfront(#{source}): cached asset_id #{asset_id}"
           asset_id
         else
           path = File.join(config.assets_dir, source)
           rewrite_path = File.exist?(path) && !CloudfrontAssetHost.disable_cdn_for_source?(source)
           asset_id = rewrite_path ? CloudfrontAssetHost.key_for_path(path) : ''
 
-          puts "rails_asset_id_with_cloudfront(#{source}): asset_id #{asset_id}"
           if @@cache_asset_timestamps
             @@asset_timestamps_cache_guard.synchronize do
               @@asset_timestamps_cache[source] = asset_id
@@ -28,7 +26,6 @@ module ActionView
       # Override asset_path so it prepends the asset_id
       def rewrite_asset_path_with_cloudfront(source, path=nil)
         asset_id = rails_asset_id(source)
-        puts "rewrite_asset_path_with_cloudfront: source: #{source}, asset_id #{asset_id}"
         if asset_id.blank?
           source
         else
